d5a9025860418603def3c3994d309c0279484da3
SeliSeptian <selisuhaeliseptian25@gmail.com>
2021-10-07 12:27:03 +0700

    Menyelesaikan video ke 4 untuk mempelajari Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Sandhika Galih",
         "email" => "sandhikagalih@unpas.ac.id",
         "image" => "sandhika.jpeg"
@@ -26,5 +29,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, aperiam! Quia quaerat totam amet eaque nobis, qui soluta voluptatibus suscipit aspernatur dignissimos repellendus molestias consequatur deserunt quos sunt ab impedit incidunt voluptatem illum repellat quas natus. Error in illo nostrum repellat quo architecto officia maiores quas, a, tempore modi explicabo officiis fugit veritatis temporibus culpa pariatur fugiat tempora commodi ex quibusdam laboriosam iusto optio alias? Aliquid, eaque a. Cupiditate rerum magni distinctio voluptate numquam ratione quisquam eum vitae minima culpa."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius cum incidunt illo molestias, amet at quos mollitia soluta fugiat totam. Nesciunt possimus fugit enim ex quidem et nihil! Deserunt iste at quo, saepe rerum sint debitis repudiandae dolorem nostrum odio quas ab architecto animi, molestiae omnis, minus harum atque ea? Odio quod eveniet accusamus soluta minima nam, ad officia corrupti itaque sunt totam aperiam, deserunt, possimus cupiditate illum voluptatibus beatae iure quia suscipit! Reprehenderit blanditiis pariatur odio. Aliquam consectetur quod vel, aliquid blanditiis impedit laboriosam in debitis inventore, repellat iusto consequuntur tenetur maiores numquam libero quibusdam perspiciatis eveniet aut est?"
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_post
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, aperiam! Quia quaerat totam amet eaque nobis, qui soluta voluptatibus suscipit aspernatur dignissimos repellendus molestias consequatur deserunt quos sunt ab impedit incidunt voluptatem illum repellat quas natus. Error in illo nostrum repellat quo architecto officia maiores quas, a, tempore modi explicabo officiis fugit veritatis temporibus culpa pariatur fugiat tempora commodi ex quibusdam laboriosam iusto optio alias? Aliquid, eaque a. Cupiditate rerum magni distinctio voluptate numquam ratione quisquam eum vitae minima culpa."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius cum incidunt illo molestias, amet at quos mollitia soluta fugiat totam. Nesciunt possimus fugit enim ex quidem et nihil! Deserunt iste at quo, saepe rerum sint debitis repudiandae dolorem nostrum odio quas ab architecto animi, molestiae omnis, minus harum atque ea? Odio quod eveniet accusamus soluta minima nam, ad officia corrupti itaque sunt totam aperiam, deserunt, possimus cupiditate illum voluptatibus beatae iure quia suscipit! Reprehenderit blanditiis pariatur odio. Aliquam consectetur quod vel, aliquid blanditiis impedit laboriosam in debitis inventore, repellat iusto consequuntur tenetur maiores numquam libero quibusdam perspiciatis eveniet aut est?"
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+
+    ]);
 });
